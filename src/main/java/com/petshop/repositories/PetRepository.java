b898b6ba8281a54645f94e4819a2c3e308e9d04a
igorpardinho <pardinhoigor@gmail.com>
2024-06-26 03:15:48 -0300

    create repository PetRepository

---
+++ src/main/java/com/petshop/repositories/PetRepository.java
@@ -0,0 +1,11 @@
+package com.petshop.repositories;
+
+import com.petshop.domain.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, UUID> {
+}
