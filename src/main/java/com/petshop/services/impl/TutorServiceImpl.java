735f2e31f38c4cbe422ecdfd03973fd4dc67f233
igorpardinho <pardinhoigor@gmail.com>
2024-07-25 23:00:26 -0300

    creation class TutorServiceImpl

---
+++ src/main/java/com/petshop/services/impl/TutorServiceImpl.java
@@ -0,0 +1,36 @@
+package com.petshop.services.impl;
+
+import com.petshop.domain.Tutor;
+import com.petshop.repositories.TutorRepository;
+import com.petshop.services.TutorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TutorServiceImpl implements TutorService {
+
+    private final TutorRepository tutorRepository;
+
+    public TutorServiceImpl(TutorRepository tutorRepository) {
+        this.tutorRepository = tutorRepository;
+    }
+
+
+    @Override
+    public Tutor findById(UUID id) {
+        Optional<Tutor> optionalTutor = tutorRepository.findById(id);
+
+        return optionalTutor.orElse(null);
+
+    }
+
+    @Override
+    public List<Tutor> findAll() {
+        return tutorRepository.findAll();
+    }
+
+
+}
