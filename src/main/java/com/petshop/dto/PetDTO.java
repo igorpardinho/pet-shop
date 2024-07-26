759b507da785561cb7a2ed22fdc5fa023d553718
igorpardinho <pardinhoigor@gmail.com>
2024-07-25 22:53:46 -0300

    add attribute Tutor

--- src/main/java/com/petshop/dto/PetDTO.java
+++ src/main/java/com/petshop/dto/PetDTO.java
@@ -1,6 +1,7 @@
 package com.petshop.dto;
 
 
+import com.petshop.domain.Tutor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,5 +20,6 @@ public class PetDTO {
     private String name;
     private String gender;
     private float weight;
+    private Tutor tutor;
 
 }
