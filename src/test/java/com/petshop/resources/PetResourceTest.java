9015d257cc18e187e9e96911a347e9e78bb6d275
igorpardinho <pardinhoigor@gmail.com>
2024-08-01 18:32:44 -0300

    add structure of tests PetResource

---
+++ src/test/java/com/petshop/resources/PetResourceTest.java
@@ -0,0 +1,33 @@
+package com.petshop.resources;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
